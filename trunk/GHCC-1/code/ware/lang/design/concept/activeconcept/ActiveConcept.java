beff67bc6a3e2524b4485755c82cd1c280a6d285
yanchangyou@gmail.com <yanchangyou@gmail.com@3cd18558-5f1d-e4c5-32b5-ff82cb6f5bcb>
2010-08-19 17:08:49 +0000

    严长友:GHCC-1-> 1，修改 ActiveConcept中错别字 2，添加一个ether实现， 结构调整完成， 内容待填充
    3，实现规范
      3.1，包名 ： ware.lang.impl.{实现名}.  如：ware.lang.impl.ether.
      3.2，类名 ： {实现名}$接口名.java   如： Ether$Ware
      3.3，原则 ：继承父类，实现接口   如：Ether$Ware extends Ether$ActiveConcept implements Ware

--- trunk/GHCC-1/code/ware/lang/design/concept/activeconcept/ActiveConcept.java
+++ trunk/GHCC-1/code/ware/lang/design/concept/activeconcept/ActiveConcept.java
@@ -4,7 +4,7 @@ import ware.lang.design.concept.Concept;
 import ware.lang.design.drive.Drive;
 
 /**
- * 能的概念
+ * 能动概念
  * @author yanchangyou
  * @date 2010-08-19 21:58:02
  * @version 1.0.0
---
+++ trunk/GHCC-1/code/ware/lang/impl/ether/concept/Ether$Concept.java
@@ -0,0 +1,19 @@
+package ware.lang.impl.ether.concept;
+
+import ware.lang.design.concept.Concept;
+import ware.lang.impl.ether.node.Ether$Node;
+
+
+
+/**
+ * 概念接口<br>
+ * 所有概念的父接口
+ * @author yanchangyou
+ * @date 2010-08-20 00:59:52
+ * @version 1.0.0
+ *
+ */
+public class Ether$Concept extends Ether$Node implements Concept {
+
+
+}
